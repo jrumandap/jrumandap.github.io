9e676d0311a276628df486df55f4565f92dc805c
Jay-R Umandap <jayrumandap0@gmail.com>
2026-05-13 16:00:17 +0800

    Update

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ function showSection(sectionId) {
 
 
 window.onload = () => showSection('about');
+
+const toggle = document.getElementById("themeToggle");
+const icon = document.querySelector("#themeToggle i");
+
+function setTheme(theme) {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+
+    // icon switch
+    if (theme === "dark") {
+        icon.classList.remove("bi-sun-fill");
+        icon.classList.add("bi-moon-stars-fill");
+    } else {
+        icon.classList.remove("bi-moon-stars-fill");
+        icon.classList.add("bi-sun-fill");
+    }
+}
+
+toggle.addEventListener("click", () => {
+    const current = document.documentElement.getAttribute("data-theme");
+
+    if (current === "light") {
+        setTheme("dark");
+    } else {
+        setTheme("light");
+    }
+});
+
+const saved = localStorage.getItem("theme") || "light";
+setTheme(saved);
